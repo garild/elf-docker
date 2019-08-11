3bc9fb0e1838c95632c6783ee76a3222ba5cc1bd
Garib <garibo@o2.pl>
2019-08-11 21:28:52 +0200

    New repo ELK - test

---
+++ ServiceLog/Controllers/LogController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ServiceLog.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class LogController
+    {
+        private readonly ILogger _logger;
+
+        public LogController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<LogController>();
+        }
+
+        [HttpGet]
+        public void Error()
+        {
+            for(int i = 0; i < 1000; i++)
+            {
+                _logger.LogError($"This is ERRRORRRRRRRRRRR {DateTime.Today}");
+            }
+        }
+
+        [HttpGet]
+        public void Info()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                _logger.LogInformation($"This is info {DateTime.Today}");
+            }
+            
+        }
+    }
+}
---
+++ ServiceLog/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using NLog;
+using NLog.Web;
+//using Serilog;
+//using Serilog.Sinks.Elasticsearch;
+//using System;
+
+namespace ServiceLog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseNLog();
+
+                // SERILOG 
+                //.UseSerilog((context, config) =>
+                //{
+                //    var elasticUri = context.Configuration["ElasticSearchUri"]; // this should be add in EVN_VARIABLES
+
+                //    config.Enrich.FromLogContext()
+                //    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri))
+                //    {
+                //        AutoRegisterTemplate = true,
+                //    });
+                //});
+
+        }
+    }
+}
